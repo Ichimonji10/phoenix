b840a54b317a9f0c9a6cd6d4425f3d98e5116c99
Jeremy Audet <ichimonji10@gmail.com>
2013-10-06 21:26:14 -0400

    Add documentation for xrndbuff.h
    Add documentation which gives a conceptual framework for thinking about the
    functions declared in `xrndbuff.h`.

--- src/kernel/xrndbuff.h
+++ src/kernel/xrndbuff.h
@@ -15,6 +15,25 @@ Please send comments or bug reports to
     PChapin@vtc.vsc.edu
 ****************************************************************************/
 
+/*! \file xrndbuff.h Circular buffer holding process information.
+ *
+ * A ringbuffer is used to store information about processes, and the functions
+ * defined here are used to interact with that ringbuffer. The ringbuffer is of
+ * a fixed size, and each process fills one element of that ringbuffer.
+ * Therefore, not all elements of the ringbuffer are filled with useful
+ * information at all times.
+ *
+ * `add_process()` should be called before any other function, or else undefined
+ * behaviour will occur. When `add_process()` is called for the first time, an
+ * index is initialized which points to that process in the ringbuffer. Other
+ * functions in this file (e.g. `get_next()`) make use of the index. The word
+ * "current" (like in `get_current()`) refers to the process being pointed to by
+ * the index.
+ *
+ * The index is maintained internally, and users of this library should not
+ * manipulate it.
+ */
+
 #ifndef XROUNDBUFF_H
 #define XROUNDBUFF_H
 
